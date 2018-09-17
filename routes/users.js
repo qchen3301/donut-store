aa604e0b2374d569c5497d9c5c68760afada0444
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-17 19:34:12 -0400

    tooled server.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const { Donut } = require('../db/schema.js')
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
